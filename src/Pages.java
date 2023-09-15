44769b6db3565fa15d4f5c96888db974d726fdca
Jerry998866 <jerryh2026@student.cis.edu.hk>
2023-09-15 16:55:21 +0800

    Added Page section

--- src/Pages.java
+++ src/Pages.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Pages {
 
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your age:");
-        int UserAge=scanner.nextInt();
-        int readPage=100-UserAge;
+        int UserAge = scanner.nextInt();
+        int readPage = 100-UserAge;
         System.out.println(UserAge+"-years olds should read at least "+readPage+" pages before giving up on a book");
 
     }
